eb65d6ac09430fb04396244aba69c2d6bc193e34
MT2017001 Abhijith M <Abhijith.M@iiitb.org>
2018-01-24 17:05:07 +0530

    Added datalink layer, replace the DBUtils password field with your DB password

---
+++ SafeNav_DLL/DBSQLServerUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Tutorial.SqlConn
+{
+    class DBSQLServerUtils
+    {
+
+        public static SqlConnection GetDBConnection(string datasource, string database, string username, string password)
+        {
+            string connString = String.Format(ConfigurationManager.ConnectionStrings["SafeNavConnectionString"].ConnectionString,datasource, database, username, password);
+            SqlConnection conn = new SqlConnection(connString);
+            return conn;
+        }
+
+
+    }
+}
---
+++ SafeNav_DLL/DBUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Tutorial.SqlConn
+{
+    class DBUtils
+    {
+        public static SqlConnection GetDBConnection()
+        {
+            string datasource = @".\sqlexpress";
+            string database = "safenav";
+            string username = "sa";
+            string password = "SAMPLEPASS";
+
+            return DBSQLServerUtils.GetDBConnection(datasource, database, username, password);
+        }
+    }
+
+}
