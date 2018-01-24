9212a54c9ab49b344acb97cd08503b8254434a42
MT2017001 Abhijith M <Abhijith.M@iiitb.org>
2018-01-24 19:21:50 +0530

    Added a dumb Intelligence to predict route

--- RouteDetails/GoogleGeoCodeData.cs
+++ RouteDetails/GoogleGeoCodeData.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using Newtonsoft.Json;
+using SafeNav_DLL;
 
 namespace RouteDetails
 {
@@ -31,7 +33,55 @@ namespace RouteDetails
 
         public List<int> GetRouteSafety()
         {
-            return null;
+            RouteInformation mapdata = JsonConvert.DeserializeObject<RouteInformation>(json);
+            return ParseStreetSafety(mapdata);
+        }
+
+        private List<int> ParseStreetSafety(RouteInformation mapdata)
+        {
+            HashSet<StreetDetails> streets = new HashSet<StreetDetails>();
+            int i = 0;
+            List<int> counter = new List<int>();
+            foreach (var route in mapdata.routes)
+            {
+                counter.Add(0);
+                foreach (var leg in route.legs)
+                {
+                    foreach (var step in leg.steps)
+                    {
+                        List<StreetDetails> stNames = Utilities.GetStreets(step);
+                        foreach (var item in stNames)
+                        {
+                            counter[i] += GetAccidentCount(item.stName);
+                        }
+                    }
+                }
+                i++;
+            }
+            return counter;
+        }
+
+        int GetAccidentCount(string stName)
+        {
+            try
+            {
+                SqlConnection conn = DBUtils.GetDBConnection();
+                int count = 0;
+                string cmd = "select AccidentCount from AccidentArea where StreetName = @stName";
+                SqlCommand oCmd = new SqlCommand(cmd, conn);
+                oCmd.Parameters.AddWithValue("@stName", stName);
+                conn.Open();
+                SqlDataReader dr = oCmd.ExecuteReader();
+                dr.Read();
+                count += dr.GetInt32(0);
+                conn.Close();
+
+                return count;
+            }
+            catch
+            {
+                return 0;
+            }
         }
 
         public List<StreetDetails> ParseStreetInfo(RouteInformation mapdata)
